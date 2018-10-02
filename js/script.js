d07376ec2329df7f04b95224f23306e79cd72318
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-03 01:31:42 +0200

    Generate the images's sizes with the data-type attribute

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const IMAGES = document.querySelectorAll("img");
+const IMAGES = document.querySelectorAll("img"),
+      SIZES = {
+          showcase: "100vw",
+          reason: "(max-width: 799px) 100vw, 372px",
+          feature: "(max-width: 799px) 100vw, 558px",
+          story: "(max-width: 799px) 100vw, 670px"
+      };
 
 function makeSrcset(imgSrc) {
     let markup = [],
         width = 400;
-    
+
     for (let i = 0; i < 5; i++) {
         markup[i] = imgSrc + width + ".jpg " + width + "w";
         width += 400;
@@ -14,6 +20,7 @@ function makeSrcset(imgSrc) {
 for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src").slice(0, -7),
         type = IMAGES[i].getAttribute("data-type"),
-        srcSet = makeSrcset(imgSrc);
-    console.log(imgSrc, "\n", type, "\n", srcSet);
+        srcSet = makeSrcset(imgSrc),
+        sizes = SIZES[type];
+    console.log(imgSrc, "\n", type, "\n", srcSet, "\n", sizes);
 }
