7e0e36b8693dc1bc1b6063ebd87503c18e7c83f2
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-03 01:13:27 +0200

    Generate the srcset attribute for all images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const IMAGES = document.querySelectorAll("img");
 
+function makeSrcset(imgSrc) {
+    let markup = [],
+        width = 400;
+    
+    for (let i = 0; i < 5; i++) {
+        markup[i] = imgSrc + width + ".jpg " + width + "w";
+        width += 400;
+    }
+    return markup.join();
+}
+
 for (let i = 0; i < IMAGES.length; i++) {
-    let imgSrc = IMAGES[i].getAttribute("src").slice(0, -8),
-        type = IMAGES[i].getAttribute("data-type");
-    console.log(imgSrc, "\n", type);
+    let imgSrc = IMAGES[i].getAttribute("src").slice(0, -7),
+        type = IMAGES[i].getAttribute("data-type"),
+        srcSet = makeSrcset(imgSrc);
+    console.log(imgSrc, "\n", type, "\n", srcSet);
 }
